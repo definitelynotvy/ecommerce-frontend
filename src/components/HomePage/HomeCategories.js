707fc63f423bc1e7d4a9f3199028e51cdc876c2b
Quangvy <nguyenquangvy6058@gmail.com>
2023-12-07 12:55:01 +0700

    Revert "change image"
    This reverts commit 54d8c1a371fe40a3121276574eb0e03d5a073023.

--- src/components/HomePage/HomeCategories.js
+++ src/components/HomePage/HomeCategories.js
@@ -38,7 +38,7 @@ const HomeCategories = () => {
                     className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
                   />
                   <span className="relative mt-auto text-center text-xl font-bold text-white">
-                    {category.name} ({category.products?.length})
+                    {category.name} ({category.products.length})
                   </span>
                 </Link>
               ))}
